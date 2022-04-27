a8f878082e8e7d740f8610724461c9077052ef42
liupanfeng <liupanfeng529@163.com>
2022-04-27 19:45:12 +0800

    增加文件夹删除

--- app/src/test/java/com/example/androidstudyfinaledition/ftp/FtpTest.java
+++ app/src/test/java/com/example/androidstudyfinaledition/ftp/FtpTest.java
@@ -115,6 +115,12 @@ public class FtpTest {
 
     }
 
+    @Test
+    public void testRemoveDir(){
+        String zipDirPath ="/home/ms/kehu/assets";
+        deleteAllFile(zipDirPath);
+    }
+
     /**
      * 添加压缩方法
      */
@@ -131,5 +137,75 @@ public class FtpTest {
     }
 
 
+    @Test
+    public void testFileName(){
+        String zipFileName ="/home/ms/kehu/assets_master.zip";
+        File file=new File(zipFileName);
+        System.out.println("file.name=" + file.getName());
+
+        String zipDirPath ="/home/ms/kehu/assets";
+        File file2=new File(zipDirPath);
+        String parent = file2.getParent();
+        deleteAllFile(zipDirPath);
+        System.out.println("file2.getParent()=" + parent);
+
+    }
+
+
+    public static boolean deleteAllFile(String dir) {
+        // 如果dir不以文件分隔符结尾，自动添加文件分隔符
+        File dirFile = new File(dir);
+        // 如果dir对应的文件不存在，或者不是一个目录，则退出
+        if ((!dirFile.exists()) || (!dirFile.isDirectory())) {
+            System.out.println("删除文件夹失败：" + dir + "不存在！");
+            return false;
+        }
+        boolean flag = true;
+        // 删除文件夹中的所有文件包括子文件夹
+        File[] files = dirFile.listFiles();
+        for (int i = 0; i < files.length; i++) {
+            // 删除子文件
+            if (files[i].isFile()) {
+                flag = deleteFile(files[i].getAbsolutePath());
+                if (!flag)
+                    break;
+            }
+            // 删除子文件夹
+            else if (files[i].isDirectory()) {
+                flag = deleteAllFile(files[i].getAbsolutePath());
+                if (!flag)
+                    break;
+            }
+        }
+        if (!flag) {
+            System.out.println("删除文件夹失败！");
+            return false;
+        }
+        // 删除当前文件夹
+        if (dirFile.delete()) {
+            System.out.println("删除文件夹" + dir + "成功！");
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public static boolean deleteFile(String fileName) {
+        File file = new File(fileName);
+        // 如果文件路径只有单个文件
+        if (file.exists() && file.isFile()) {
+            if (file.delete()) {
+                System.out.println("删除文件" + fileName + "成功！");
+                return true;
+            } else {
+                System.out.println("删除文件" + fileName + "失败！");
+                return false;
+            }
+        } else {
+            System.out.println(fileName + "不存在！");
+            return false;
+        }
+    }
 
 }
